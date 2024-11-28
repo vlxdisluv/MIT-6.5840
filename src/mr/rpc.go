383c417f8c24cb0792ec17bbb7ebbe66d0f3c467
vlxdisluv <vlxdisluv@gmail.com>
2024-11-28 23:26:50 +0500

    feat: prepare coordinator to fetch available map tasks via RPC

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -1,29 +1,23 @@
 package mr
 
-//
-// RPC definitions.
-//
-// remember to capitalize all names.
-//
-
 import "os"
 import "strconv"
 
-//
-// example to show how to declare the arguments
-// and reply for an RPC.
-//
+type TaskType int
 
-type ExampleArgs struct {
-	X int
+const (
+	TaskMap TaskType = iota
+	TaskReduce
+)
+
+type GetTaskArgs struct {
 }
 
-type ExampleReply struct {
-	Y int
+type GetTaskReply struct {
+	FileName   string
+	TaskType   TaskType
+	TaskNumber int
 }
-
-// Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,77 +1,77 @@
 package mr
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"sort"
+	"time"
+)
+
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
+type ByKey []KeyValue
 
-//
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	for {
+		taskReply, err := GetTask()
 
-	// Your worker implementation here.
+		if err != nil {
+			fmt.Printf("error getting file name: %v\n", err)
+			break
+		}
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+		content, err := os.ReadFile(taskReply.FileName)
+		if err != nil {
+			log.Fatalf("cannot open %v", taskReply.FileName)
+		}
+		kva := mapf(taskReply.FileName, string(content))
 
-}
+		fmt.Printf("Processing file: %v\n", len(kva))
 
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
+		sort.Sort(ByKey(kva))
+		
+		time.Sleep(5 * time.Second)
 	}
 }
 
-//
+func GetTask() (*GetTaskReply, error) {
+	args := &GetTaskArgs{}
+	reply := &GetTaskReply{}
+
+	ok := call("Coordinator.GetTask", args, reply)
+
+	if !ok {
+		return nil, fmt.Errorf("task not found")
+	}
+
+	return reply, nil
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
